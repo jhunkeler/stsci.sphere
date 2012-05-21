36d67e4de8460a57014f08432ced33b6d9e07cfd
mdroe <mdroe@stsci.edu>
2012-05-21 18:34:16 +0000

    Fix resolve_image name to work when uncompressed image is not around.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/branches/sphere@16924 fe389314-cf27-0410-b35b-8c050e845b92
    
    Former-commit-id: fa6bffe6aec60a178ef39a39ddcb7376768f7f10

--- lib/test/test_shared.py
+++ lib/test/test_shared.py
@@ -6,7 +6,7 @@ def resolve_imagename(ROOT_DIR, base_name):
     image_name = os.path.join(ROOT_DIR, base_name)
 
     # Is it zipped?
-    if not os.path.isfile(image_name):
-        image_name.replace('.fits', '.fits.gz')
+    if not os.path.exists(image_name):
+        image_name = image_name.replace('.fits', '.fits.gz')
 
     return image_name
