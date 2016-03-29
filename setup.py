a4c23f3c595a19dad8e3ff1341b6d0b0ff061171
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-29 11:29:46 -0400

    Implement RECON
    Former-commit-id: 5a4ad567d57fb06aa53d375da27d737b0fa7ead8

--- setup.py
+++ setup.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python
+import recon.release
+from glob import glob
+from numpy import get_include as np_include
+from setuptools import setup, find_packages, Extension
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+version = recon.release.get_info()
+recon.release.write_template(version, 'stsci/sphere')
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3.dev'],
-    namespace_packages=['stsci'], packages=['stsci'],
-    d2to1=True,
-    use_2to3=True,
-    zip_safe=False
+    name = 'stsci.sphere',
+    version = version.pep386,
+    author = 'Michael Droettboom',
+    author_email = 'help@stsci.edu',
+    description = 'Python based tools spherical geometry',
+    url = 'https://github.com/spacetelescope/stsci.sphere',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'astropy',
+        'nose',
+        'numpy',
+        'sphinx',
+        'stsci.sphinxext'
+    ],
+    packages = find_packages(),
+    package_data = {
+        '': ['LICENSE.txt'],
+        'stsci/sphere/test': ['data/*']
+    },
+    ext_modules=[
+        Extension('stsci.sphere.math_util',
+            glob("src/*.c"),
+            include_dirs=[np_include()],
+            define_macros=[('NUMPY', '1')]
+        ),
+    ],
 )
-
-#     package_data =   {'stsci.sphere.test': ['data/*.fits', 'data/*.fits.gz']},
-
--- stsci/sphere/__init__.py
+++ stsci/sphere/__init__.py
@@ -1,4 +1,5 @@
 import sys
+from .version import *
 
 if sys.version_info[0] >= 3:
     # Python 3 compatibility
